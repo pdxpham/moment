49a300c42a061bea1813ba38a5fefe9164ed50f3
Quang PHAM <46787469+pdxpham@users.noreply.github.com>
2019-01-21 18:24:32 +0100

    Update moment.js

--- moment.js
+++ moment.js
@@ -1833,7 +1833,7 @@
             try {
                 oldLocale = globalLocale._abbr;
                 var aliasedRequire = require;
-                aliasedRequire('./locale/' + name);
+                //aliasedRequire('./locale/' + name);
                 getSetGlobalLocale(oldLocale);
             } catch (e) {}
         }
